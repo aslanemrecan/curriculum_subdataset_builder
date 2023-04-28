27e0b5418a89b4a0441e6e7a5cdb2d804e2d4038
emrecaslan <aslane84@students.rowan.edu>
2023-04-28 14:54:38 -0400

    Dividing dataset by size stats

--- dataset_builder.py
+++ dataset_builder.py
@@ -3,10 +3,30 @@ import os
 import shutil
 import pandas as pd
 
+def create_directories(dir_names, dir_paths):
+    for dir_path in dir_paths:
+        for dir_name in dir_names:
+            full_dir_path = os.path.join(dir_path, dir_name)
+            if not os.path.exists(full_dir_path):
+                os.makedirs(full_dir_path)
+
+def copy_files(paths_dict, size_path):
+    for idx, file_path in paths_dict.items():
+        filename = os.path.splitext(os.path.basename(file_path))[0]
+        shutil.copy(file_path, os.path.join(size_path, "labels", filename + ".txt"))
+        shutil.copy(file_path.replace("labels", "images").replace("txt", "jpg"), os.path.join(size_path, "images", filename + ".jpg"))
+
+def copy_all_files(small_paths, medium_paths, large_paths, small_path, medium_path, large_path):
+    copy_files(small_paths, small_path)
+    copy_files(medium_paths, medium_path)
+    copy_files(large_paths, large_path)
+
 # Settings
 #stat_path = "STATS_FOR__data_Koutsoubn8_ijcnn_v7data_high_density_100k_train.csv"
-stat_path = "STATS_FOR__data_Koutsoubn8_ijcnn_v7data_single_label_set_train.csv"
-save_path = "data/"
+#stat_path = "STATS_FOR__data_Koutsoubn8_ijcnn_v7data_single_label_set_train.csv"
+#stat_path = "STATS_FOR__data_Koutsoubn8_Real_world_test_images.csv"
+stat_path = "/home/aslane84/dataset_builder/STATS_FOR__home_aslane84_dataset_builder_dataset_builder_validation_mainset.csv"
+save_path = "val_data/"
 
 # Read the CSV file into a pandas dataframe
 df = pd.read_csv(stat_path)
@@ -23,25 +43,8 @@ small_path = os.path.join(save_path, "small")
 medium_path = os.path.join(save_path, "medium")
 large_path = os.path.join(save_path, "large")
 
-def create_directories(dir_names, dir_paths):
-    for dir_path in dir_paths:
-        for dir_name in dir_names:
-            full_dir_path = os.path.join(dir_path, dir_name)
-            if not os.path.exists(full_dir_path):
-                os.mkdir(full_dir_path)
-
 dir_names = ["labels", "images"]
 dir_paths = [small_path, medium_path, large_path]
 
 create_directories(dir_names, dir_paths)
-
-def copy_files(paths_dict, size_path):
-    for idx, file_path in paths_dict.items():
-        filename = os.path.splitext(os.path.basename(file_path))[0]
-        shutil.copy(file_path, os.path.join(size_path, "labels", filename + ".txt"))
-        shutil.copy(file_path.replace("labels", "images").replace("txt", "jpg"), os.path.join(size_path, "images", filename + ".jpg"))
-
-def copy_all_files(small_paths, medium_paths, large_paths, small_path, medium_path, large_path):
-    copy_files(small_paths, small_path)
-    copy_files(medium_paths, medium_path)
-    copy_files(large_paths, large_path)
+copy_all_files(small_paths, medium_paths, large_paths, small_path, medium_path, large_path)
